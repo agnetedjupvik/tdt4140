a693cdadd71b519db8f06969462afa3119a811c1
simen <simennj@outlook.com>
2016-02-08 16:34:41 +0100

    add test json file to project

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,7 +6,7 @@ import java.nio.file.Paths;
 
 public class test {
     public static void main(String[] args) {
-        Path path = Paths.get("downtown-crosstown.json");
+        Path path = Paths.get("src/main/resources/downtown-crosstown.json");
         try(BufferedReader reader = Files.newBufferedReader(path)) {
             while(reader.ready()) {
                 System.out.println(reader.readLine());
