879e675c120dbbe361ab26a875fd733b5b2eb3b9
simen <simennj@outlook.com>
2016-02-08 16:29:47 +0100

    add prototype jsonreading

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,2 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class test {
+    public static void main(String[] args) {
+        Path path = Paths.get("downtown-crosstown.json");
+        try(BufferedReader reader = Files.newBufferedReader(path)) {
+            while(reader.ready()) {
+                System.out.println(reader.readLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
