4e9241aaa5d0928424e3cf0dd55e62deb8b57cbd
Simen <Simennj@outlook.com>
2016-02-21 15:45:35 +0100

    add proof of concept REST consumer in angular

--- src/main/java/rest/Controller.java
+++ src/main/java/rest/Controller.java
@@ -5,6 +5,7 @@ import input.Speedpoint;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class Controller {
@@ -12,6 +13,11 @@ public class Controller {
     InputSimulator inputSimulator = new InputSimulator("src/main/resources/downtown-crosstown.json");
 
     @RequestMapping("/")
+    public ModelAndView rootRedirect() {
+        return new ModelAndView("redirect:/speedometer.html");
+    }
+
+    @RequestMapping("/api")
     public Speedpoint greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return inputSimulator.getLatestSpeedpoint();
     }
