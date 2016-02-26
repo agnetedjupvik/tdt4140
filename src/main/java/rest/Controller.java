95319e0979f74c3f6e2de2f2fbc851f78b08950c
Espen Meidell <espen.meidell@gmail.com>
2016-02-26 15:55:39 +0100

    En del grafgreier fungerer

--- src/main/java/rest/Controller.java
+++ src/main/java/rest/Controller.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 public class Controller {
 
@@ -21,4 +26,16 @@ public class Controller {
     public Speedpoint greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return inputSimulator.getLatestSpeedpoint();
     }
+
+    @RequestMapping("/api/speedhistory")
+    public HashMap speedHistory(){
+        Integer[] speeds = {10, 20, 25, 20, 40, 44, 55, 0};
+        String[] times = {LocalTime.of(12, 30).toString(), LocalTime.of(12, 32).toString(), LocalTime.of(12, 34).toString(), LocalTime.of(12, 36).toString(),
+                LocalTime.of(12, 38).toString(), LocalTime.of(12, 40).toString(), LocalTime.of(12, 42).toString(), LocalTime.of(12, 44).toString()};
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("speeds", speeds);
+        map.put("times", times);
+        return map;
+    }
+
 }
--- src/main/resources/static/js/chart.js
+++ src/main/resources/static/js/chart.js
@@ -1,14 +1,83 @@
-var app = angular.module('myApp',['chart.js']);
-
+var app = angular.module('myApp',['chart.js','ngResource']);
+/**
 app.controller('MainController', function($scope){
- $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September"];
+  $scope.series = ['Series A'];
   $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+    [65, 59, 80, 81, 56, 55, 40, 100, 100]
+  ];;
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
-
 });
+
+app.controller('SpeedController', ['$scope', '$resource', '$interval', function ($scope, $resource, $interval) {
+    var rest = $resource('/api');
+    $scope.limit = 20;
+    $interval(function () {
+        rest.get(function (data) {
+            $scope.speed = Math.round(data.value);
+            if ($scope.speed > $scope.limit + 5) {
+                $scope.status = 'bad';
+            } else if ($scope.speed > $scope.limit) {
+                $scope.status = 'okay';
+            } else {
+                $scope.status = 'good';
+            }
+        });
+    }, 200);
+}]);
+**/
+app.controller("ChartController", ["$scope", "$resource", "$interval", "$timeout", function($scope, $resource, $interval, $timeout){
+
+
+    Chart.defaults.global.scaleOverride = true;
+    Chart.defaults.global.beginAtZero = true;
+    Chart.defaults.global.animation = false;
+    Chart.defaults.global.scaleSteps = 25;
+    Chart.defaults.global.scaleStepWidth = 4;
+
+
+    /* Working static implementation
+    var rest = $resource("/api/speedhistory");
+    $scope.labels = ["", "", "", "", "", "", ""];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40]
+    ];
+    $scope.legend = true;
+    $scope.series = ["Speed Data"]
+    $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+    };
+
+
+    rest.get(function (speeddata) {
+        $scope.data = [
+            speeddata.speeds
+        ];
+        $scope.labels = speeddata.times;
+    });
+    */
+
+    //Dynamic update
+    $scope.labels = [];
+    $scope.data  = [
+        []
+        ];
+    $scope.series = ["Speed Data"]
+    var rest = $resource("/api");
+    $interval(function () {
+        rest.get(function (speedpoint) {
+            console.log("Adding data point...")
+            if ($scope.data[0].length > 30) {
+                $scope.data[0].shift();
+                $scope.labels.shift();
+            }
+            $scope.labels.push(new Date(speedpoint.time));
+            $scope.data[0].push(speedpoint.value);
+        });
+    }, 100);
+
+
+
+}]);
