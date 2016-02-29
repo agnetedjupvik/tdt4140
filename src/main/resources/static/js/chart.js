3b52ce078d63497b915ac81445b2b95f7f9c4293
Espen Meidell <espen.meidell@gmail.com>
2016-02-29 10:32:36 +0100

    Forbedringer på graf, pauseknapp

--- src/main/resources/static/js/chart.js
+++ src/main/resources/static/js/chart.js
@@ -1,33 +1,5 @@
 var app = angular.module('myApp',['chart.js','ngResource']);
-/**
-app.controller('MainController', function($scope){
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September"];
-  $scope.series = ['Series A'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40, 100, 100]
-  ];;
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-});
 
-app.controller('SpeedController', ['$scope', '$resource', '$interval', function ($scope, $resource, $interval) {
-    var rest = $resource('/api');
-    $scope.limit = 20;
-    $interval(function () {
-        rest.get(function (data) {
-            $scope.speed = Math.round(data.value);
-            if ($scope.speed > $scope.limit + 5) {
-                $scope.status = 'bad';
-            } else if ($scope.speed > $scope.limit) {
-                $scope.status = 'okay';
-            } else {
-                $scope.status = 'good';
-            }
-        });
-    }, 200);
-}]);
-**/
 app.controller("ChartController", ["$scope", "$resource", "$interval", "$timeout", function($scope, $resource, $interval, $timeout){
 
 
@@ -36,7 +8,8 @@ app.controller("ChartController", ["$scope", "$resource", "$interval", "$timeout
     Chart.defaults.global.animation = false;
     Chart.defaults.global.scaleSteps = 25;
     Chart.defaults.global.scaleStepWidth = 4;
-
+    Chart.defaults.Line.bezierCurve = false;
+    var running = true;
 
     /* Working static implementation
     var rest = $resource("/api/speedhistory");
@@ -60,25 +33,41 @@ app.controller("ChartController", ["$scope", "$resource", "$interval", "$timeout
     */
 
     //Dynamic update
-    $scope.labels = [];
+    $scope.labels = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
     $scope.data  = [
-        []
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
         ];
-    $scope.series = ["Speed Data"]
+    $scope.currentspeed = 0;
+    $scope.series = ["Speed Data (mph)"];
     var rest = $resource("/api");
     $interval(function () {
         rest.get(function (speedpoint) {
-            console.log("Adding data point...")
-            if ($scope.data[0].length > 30) {
-                $scope.data[0].shift();
-                $scope.labels.shift();
+            if (running) {
+                if ($scope.data[0].length > 29) {
+                    $scope.data[0].shift();
+                    $scope.labels.shift();
+                }
+                var date = new Date(speedpoint.time * 1000);
+                $scope.labels.push(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+                $scope.data[0].push(speedpoint.value);
+                $scope.currentspeed = speedpoint.value;
             }
-            var date = new Date(speedpoint.time*1000);
-            $scope.labels.push(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());
-            $scope.data[0].push(speedpoint.value);
         });
     }, 1000);
 
+    $scope.buttonmessage = "Stop";
+    $scope.pause = function () {
+        if (running) {
+            $scope.buttonmessage = "Start";
+        } else {
+            $scope.buttonmessage = "Stop";
+            $scope.labels = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
+            $scope.data  = [
+                [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            ];
+        }
+        running = !running;
+    };
 
 
 }]);
