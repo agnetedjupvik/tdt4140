ad5c6ad5b8e912810f64098f8fa47b362e7deaac
simen <simennj@outlook.com>
2016-03-11 16:30:48 +0100

    separate logging from application class

--- src/main/java/rest/Application.java
+++ src/main/java/rest/Application.java
@@ -11,19 +11,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class Application implements CommandLineRunner {
+public class Application {
 
     public static Input inputSimulator = new InputSimulator("downtown-crosstown.json");
-
-    @Autowired
-    Logger logger;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        logger.getSnapshot(inputSimulator);
-    }
 }
--- src/main/java/rest/Logger.java
+++ src/main/java/rest/Logger.java
@@ -4,19 +4,27 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import input.Input;
 import input.Snapshot;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class Logger {
+public class Logger implements CommandLineRunner{
+
+    Snapshot snapshot;
 
     @Async
     public void getSnapshot(Input input) throws InterruptedException, JsonProcessingException {
         while (true) {
-            Snapshot snapshot = input.getLatestDataPoints();
+            Snapshot newSnapshot = input.getLatestDataPoints();
             System.out.println(new ObjectMapper().writeValueAsString(snapshot));
             Thread.sleep(200L);
         }
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.getSnapshot(Application.inputSimulator);
     }
 }
