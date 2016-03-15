08322835c0df734c0493f91b983dd2c88234dd57
augholm <augholm@gmail.com>
2016-03-15 09:14:57 +0100

    Datapoint now converts string values to a double

--- src/main/java/input/Datapoint.java
+++ src/main/java/input/Datapoint.java
@@ -8,6 +8,7 @@ public class Datapoint {
     private final double value;
     private final double time;
 
+
     public Datapoint(Datatype datatype, double data, double time) {
         this.datatype = datatype;
         this.value = data;
@@ -19,12 +20,21 @@ public class Datapoint {
     }
 
     public static double parseValue(String string) {
-        try {
-            return Double.valueOf(string.substring(string.indexOf(':') + 1));
-        } catch (Exception e) {
-            System.out.println(string);
+        String value = string.substring(string.indexOf(':') + 1);
+        if (value.equals("true")) return 1;
+        else if (value.equals("false")) return 0;
+        else if (value.charAt(0) == '"') {
+            if (value.equals("\"reverse\"")) return -1;
+            else if (value.equals("\"neutral\"")) return 0;
+            else if (value.equals("\"first\"")) return 1;
+            else if (value.equals("\"second\"")) return 2;
+            else if (value.equals("\"third\"")) return 3;
+            else if (value.equals("\"fourth\"")) return 4;
+            else if (value.equals("\"fifth\"")) return 5;
+            else if (value.equals("\"sixth\"")) return 6;
+            else return 0;
         }
-        return 404;
+        return Double.valueOf(value);
     }
 
     public Datatype getDatatype() {
